2f6dc6e7c9d6cfff852b5772917c403042808ed8
Norhan Abdelkarim <norhanabdelkarim1@gmail.com>
2022-12-18 22:24:17 +0200

    Adding keypad code

---
+++ Real life code/Keypad code.ino
@@ -0,0 +1,28 @@
+#include <Keypad.h>
+
+//Code for a 4x3 Keypad
+#include <Keypad.h>
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+char keys[ROWS][COLS] = {
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
+};
+byte rowPins[ROWS] = {2, 3, 4, 5};
+byte colPins[COLS] = {6, 7, 8};
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  char key = keypad.getKey();
+  if (key) {
+    Serial.print(key);
+    Serial.print(" ");
+  }
+}
